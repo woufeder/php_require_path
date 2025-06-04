9eddce0a52a26de67ee7c80eed22538c60836f40
idben <idben.chen@gmail.com>
2025-06-04 08:48:40 +0800

    絕對路徑寫法

---
+++ cate1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php
+  require_once "../css.php";
+  ?>
+  <title>cate1</title>
+</head>
+<body>
+  <header></header>
+  <a href="../">back</a>
+</body>
+</html>
